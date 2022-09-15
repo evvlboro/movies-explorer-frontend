404a1cd5c5ed225dfaeab6feda37e1dbdb634221
evvlboro <be@bchc.app>
2022-09-15 15:10:15 +0300

    feat: add Preloader component

---
+++ src/components/Preloader.js
@@ -0,0 +1,11 @@
+const Preloader = () => {
+  return (
+    <div className="preloader">
+      <div className="preloader__container">
+        <span className="preloader__round"></span>
+      </div>
+    </div>
+  )
+};
+
+export default Preloader
